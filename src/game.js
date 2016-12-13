83224e0cbaa54adf170ad5cde163b29a83160f93
Qlwentt <qlwentt@gmail.com>
2016-12-13 10:49:51 -0800

    first passing test

---
+++ src/game.js
@@ -0,0 +1,38 @@
+var Board = function(){
+	this.board = [[1,2,3],[4,5,6],[7,8,9]];
+};
+
+Board.prototype ={
+	show: function(){ return this.board;}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Board;
