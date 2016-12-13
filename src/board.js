80e48db838ee9ac057cfb8dbe294bb0882a17a84
Melissa Rodriguez <olenellina@gmail.com>
2016-12-13 14:58:18 -0800

    updating board property to be boardArray to make it more clear the difference between the object and the property (tests are currently broken, as we'll need to wait on the play method to be complete and then we can update the tests)

--- src/board.js
+++ src/board.js
@@ -1,5 +1,5 @@
 var Board = function(){
-	this.board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
+	this.boardArray = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
 	this.counter = 0
 };
 
@@ -8,39 +8,39 @@ Board.prototype ={
 		// Note: there might be more functionality that we'll want to add here.
 		// Basically anything that is done during a new game setup to generate the board and give control to a player.
 		// That functionality could also be handeled by the game or whichever function calls clear()
-		this.board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
+		this.boardArray = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
 		this.counter = 0;
-		return this.board;
+		return this.boardArray;
 	},
 	show: function(){
 
-	for (let i = 0; i < this.board.length; i++) {
-		console.log(this.board[i][0].toString() + " | " + this.board[i][1].toString() + " | " + this.board[i][2].toString() );
+	for (let i = 0; i < this.boardArray.length; i++) {
+		console.log(this.boardArray[i][0].toString() + " | " + this.boardArray[i][1].toString() + " | " + this.boardArray[i][2].toString() );
 		if (i < 2) {
 			console.log("---------");
 		}
 	}
-		return this.board;
+		return this.boardArray;
 	},
-	isWon: function(board) {
+	isWon: function() {
 		this.counter += 1;
 		// horizontal row:
-		for (let i = 0; i < board.length; i++) {
-			if (checker(board[i][0], board[i][1], board[i][2])) {
-				return board[i][0];
+		for (let i = 0; i < this.boardArray.length; i++) {
+			if (checker(this.boardArray[i][0], this.boardArray[i][1], this.boardArray[i][2])) {
+				return this.boardArray[i][0];
 			}
 		}
 		// Vertical column:
-		for (let i = 0; i < board.length; i++) {
-			if (checker(board[0][i], board[1][i], board[2][i])) {
-				return board[0][i];
+		for (let i = 0; i < this.boardArray.length; i++) {
+			if (checker(this.boardArray[0][i], this.boardArray[1][i], this.boardArray[2][i])) {
+				return this.boardArray[0][i];
 			}
 		}
 		// Diagonal:
-		if (checker(board[0][0], board[1][1], board[2][2])) {
-			return board[0][0];
-		} else if (checker(board[0][2], board[1][1], board[2][0])) {
-			return board[0][2];
+		if (checker(this.boardArray[0][0], this.boardArray[1][1], this.boardArray[2][2])) {
+			return this.boardArray[0][0];
+		} else if (checker(this.boardArray[0][2], this.boardArray[1][1], this.boardArray[2][0])) {
+			return this.boardArray[0][2];
 		}
 		// Final case - checks for tie
 		if (this.counter === 9) {
