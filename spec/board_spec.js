cfcfbf542f86c78e3d1beb953831db1855bc4c4e
Melissa Rodriguez <olenellina@gmail.com>
2016-12-13 11:32:12 -0800

    second working test and corresponding code (also refactoring first test/code to ensure the winning element is returned, not just true)

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -11,11 +11,13 @@ describe('Board', function() {
         });
     });
     describe('isWon', function() {
-          it('should return true if a game has been won', function() {
+          it('should return true if a game has been won, along with the winning element', function() {
             var winningBoard = new Board;
-              // console.log("testboard:",testBoard.show());
-              expect(winningBoard.isWon([[1, 1, 1], [1, 1, 0], [0, 0, 1]])).toEqual(true);
-              // expect( Array.isArray(testBoard.show()[0]) ).toEqual(true);
+              expect(winningBoard.isWon([[1, 1, 1], [1, 1, 0], [0, 0, 1]])).toEqual(1);
+          });
+          it('should return false if a game has not been won', function() {
+            var winningBoard = new Board;
+              expect(winningBoard.isWon([[0, 1, 0], [1, 1, 0], [0, 0, 1]])).toEqual(false);
           });
       });
 });
--- src/game.js
+++ src/game.js
@@ -8,26 +8,27 @@ Board.prototype ={
 		// horizontal row:
 		for (let i = 0; i < board.length; i++) {
 			if (checker(board[i][0], board[i][1], board[i][2])) {
-				return true;
+				return board[i][0];
 			}
 		}
 		// Vertical column:
 		for (let i = 0; i < board.length; i++) {
 			if (checker(board[0][i], board[1][i], board[2][i])) {
-				return true;
+				return board[0][i];
 			}
 		}
 		// Diagonal:
 		if (checker(board[0][0], board[1][1], board[2][2])) {
-			return true;
+			return board[0][0];
 		} else if (checker(board[0][2], board[1][1], board[2][0])) {
-			return true;
+			return board[0][2];
 		}
 		// Final case
 		return false;
 	}
 };
 
+// Handles the checking of board elements
 var checker = function(element1, element2, element3) {
   return (element1 === element2 && element2 === element3);
 };
