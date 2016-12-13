d39e1515c3ea8b5d26cef3acf28ae8213f2fdfe4
Melissa Rodriguez <olenellina@gmail.com>
2016-12-13 11:16:16 -0800

    one passing test for board - checks whether a game is won

--- spec/board_spec.js
+++ spec/board_spec.js
@@ -10,4 +10,12 @@ describe('Board', function() {
             expect( Array.isArray(testBoard.show()[0]) ).toEqual(true);
         });
     });
+    describe('isWon', function() {
+          it('should return true if a game has been won', function() {
+            var winningBoard = new Board;
+              // console.log("testboard:",testBoard.show());
+              expect(winningBoard.isWon([[1, 1, 1], [1, 1, 0], [0, 0, 1]])).toEqual(true);
+              // expect( Array.isArray(testBoard.show()[0]) ).toEqual(true);
+          });
+      });
 });
--- src/game.js
+++ src/game.js
@@ -3,17 +3,34 @@ var Board = function(){
 };
 
 Board.prototype ={
-	show: function(){ return this.board;}
+	show: function(){ return this.board;},
+	isWon: function(board) {
+		// horizontal row:
+		for (let i = 0; i < board.length; i++) {
+			if (checker(board[i][0], board[i][1], board[i][2])) {
+				return true;
+			}
+		}
+		// Vertical column:
+		for (let i = 0; i < board.length; i++) {
+			if (checker(board[0][i], board[1][i], board[2][i])) {
+				return true;
+			}
+		}
+		// Diagonal:
+		if (checker(board[0][0], board[1][1], board[2][2])) {
+			return true;
+		} else if (checker(board[0][2], board[1][1], board[2][0])) {
+			return true;
+		}
+		// Final case
+		return false;
+	}
 };
 
-
-
-
-
-
-
-
-
+var checker = function(element1, element2, element3) {
+  return (element1 === element2 && element2 === element3);
+};
 
 
 
